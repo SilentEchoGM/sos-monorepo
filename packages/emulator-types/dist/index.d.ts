dfda4697e1761c46db12d4fc4b097d79dc91ad7a
Eoin Bathurst <silentechouk@gmail.com>
2022-03-21 02:28:05 +0100

    import emulator-types

---
+++ packages/emulator-types/dist/index.d.ts
@@ -0,0 +1,32 @@
+import type { SOS } from "sos-plugin-types";
+export declare namespace SOSEmulator {
+    interface FrontendToBackendEvents {
+        "send-packet": (packet: SOS.Packet) => void;
+        "close-wss": () => void;
+        "open-wss": () => void;
+        "load-playback": (gameId: string) => void;
+        "start-playback": () => void;
+        "stop-playback": () => void;
+        "set-playback-current-frame": (currentFrame: number) => void;
+        "get-playback-library": () => void;
+        "start-recording": () => void;
+        "stop-recording": () => void;
+    }
+    interface BackendToFrontendEvents {
+        "wss-closed": () => void;
+        "wss-open": () => void;
+        "wss-port-busy": () => void;
+        "playback-started": () => void;
+        "playback-stopped": () => void;
+        "playback-library": (library: string[]) => void;
+        "playback-length": (length: number) => void;
+        "playback-loaded": (length: number) => void;
+        "playback-current-frame": (currentFrame: number) => void;
+        "playback-load-error": () => void;
+        "recording-started": () => void;
+        "recording-stopped": () => void;
+        "recording-no-server": () => void;
+        initial: () => void;
+    }
+}
+//# sourceMappingURL=index.d.ts.map
