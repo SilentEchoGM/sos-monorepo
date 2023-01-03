6f0fd335f7e6636df1313ca5b01b2713d233598e
Eoin Bathurst <silent@silentecho.eu>
2023-01-03 03:45:17 +0100

    better data shape

--- apps/emulator-svelte/src/packetFactory/getBallHit.ts
+++ apps/emulator-svelte/src/packetFactory/getBallHit.ts
@@ -1,23 +1,25 @@
-import { gameId } from "../stores";
 import type { SOS } from "sos-plugin-types";
 import { get } from "svelte/store";
+import { gameId } from "../stores";
+import type { Options } from "../types";
 import { getLocation } from "./utils/getLocation";
 import { getPlayer } from "./utils/getPlayer";
-import type { Options } from "../types";
 
 export const getBallHit = ({
   match_guid = get(gameId),
   player = getPlayer(),
-}: Options) =>
-  ({
-    data: {
-      ball: {
-        location: getLocation(),
-        post_hit_speed: 10,
-        pre_hit_speed: 1,
-      },
-      match_guid,
-      player,
+}: Options): SOS.GameBallHit => ({
+  data: {
+    ball: {
+      location: getLocation(),
+      post_hit_speed: 10,
+      pre_hit_speed: 1,
     },
-    event: "game:ball_hit",
-  } as SOS.GameBallHit);
+    match_guid,
+    player: {
+      id: player.id,
+      name: player.name,
+    },
+  },
+  event: "game:ball_hit",
+});
